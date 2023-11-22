e2ea6799212d00ca4ad77bb5999f8f6ad48f2d66
Swarup Sapkota <empswarupcoc@gmail.com>
2023-11-22 15:22:02 +0545

    Main branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,23 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  function decCount() {
-    setCount(count - 1);
-  }
-
-  function incCount() {
-    setCount(count + 1);
-  }
-
   return (
     <div className="container">
-      <h1 className="title">Counter</h1>
-      <p className="desc">Number counter using useState</p>
-      <button className="counter" onClick={decCount}>
-        -
-      </button>
-      <span className="counterNo">{count}</span>
-      <button className="counter" onClick={incCount}>
-        +
-      </button>
+      <h1 className="title">Hello!</h1>
+      <p className="desc">This is used by me to learn React JS</p>
+      <img
+        src="https://media1.giphy.com/media/rgbAtUv07WAW4/giphy.gif?cid=ecf05e47w2ojn77c4nqvtlq5h34cry0ma9y2yfng9zwgchob&ep=v1_gifs_search&rid=giphy.gif&ct=g"
+        alt="GIF"
+        className="gif"
+      />
+      <a
+        href="https://github.com/EmpSwarup/learning-react"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="github-link"
+      >
+        GitHub
+      </a>
     </div>
   );
 }
